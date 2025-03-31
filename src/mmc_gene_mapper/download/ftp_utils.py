3f5dce391578ff7b2ec4acfe14c8016d25e76e47
danielsf <scott.daniel@alleninstitute.org>
2025-03-31 11:10:04 -0700

    add utilities to download files with FTP

---
+++ src/mmc_gene_mapper/download/ftp_utils.py
@@ -0,0 +1,43 @@
+"""
+Define generic FTP download utils
+"""
+import ftplib
+import json
+
+import mmc_gene_mapper.utils.timestamp as timestamp
+
+
+
+def download_files_from_ftp(
+        ftp_host,
+        file_dst_mapping,
+        metadata_dst):
+    """
+    Parameters
+    ----------
+    ftp_host:
+        string. The name of the FTP host from which to download
+        the files
+    file_dst_mapping:
+        a dict mapping the names of the files on the FTP host to
+        their destinations on the local drive
+    metadata_dst:
+        path to JSON file where metadata characterizing this
+        download will be written
+    """
+    metadata = {
+        'host': ftp_host,
+        'files': list(file_dst_mapping.keys()),
+        'downloaded_on': timestamp.get_timestamp()
+    }
+    metadata_str = json.dumps(metadata, indent=2)
+
+    host = ftplib.FTP(ftp_host)
+    host.login()
+    for src in file_dst_mapping:
+        print(f'=======DOWNLOADING {src}=======')
+        with open(file_dst_mapping[src], 'wb') as dst:
+            host.retrbinary(f'RETR {src}', dst.write)
+
+    with open(metadata_dst, 'w') as dst:
+        dst.write(metadata_str)
