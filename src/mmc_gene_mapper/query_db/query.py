8996549394d21c2806fb5e5b5ba33675389f4e00
danielsf <scott.daniel@alleninstitute.org>
2025-03-31 10:33:16 -0700

    limit query by citation

--- src/mmc_gene_mapper/query_db/query.py
+++ src/mmc_gene_mapper/query_db/query.py
@@ -6,10 +6,16 @@ def ncbi_to_ensembl(
         db_path,
         ncbi_id_list,
         species_taxon,
-        chunk_size=100):
+        chunk_size=100,
+        citation="NCBI"):
     does_path_exist(db_path)
     results = dict()
     with sqlite3.connect(db_path) as conn:
+
+        citation = get_citation(
+            conn=conn,
+            name=citation
+        )
 
         cursor = conn.cursor()
         for i0 in range(0, len(ncbi_id_list), chunk_size):
@@ -22,17 +28,45 @@ def ncbi_to_ensembl(
                 ENSEMBL_id
             FROM NCBI_to_ENSEMBL
             WHERE
+                citation=?
+            AND
                 species_taxon=?
             AND
                 NCBI_id IN {tuple(values)}
             """
-            chunk = cursor.execute(query, (species_taxon, )).fetchall()
+            chunk = cursor.execute(
+                query,
+                (citation['idx'], species_taxon)).fetchall()
             for row in chunk:
                 if row[0] not in results:
                     results[row[0]] = []
                 results[row[0]].append(row[1])
     return results
-                
+
+
+def get_citation(conn, name):
+
+    cursor = conn.cursor()
+
+    results = cursor.execute(
+        """
+        SELECT name, id, metadata
+        FROM citation
+        WHERE name=?
+        """,
+        (name,)
+    ).fetchall()
+
+    if len(results) > 1:
+        raise ValueError(
+            f"More than one citation corresponding to {name}"
+        )
+    return {
+        "name": results[0][0],
+        "idx": results[0][1],
+        "metadata": results[0][2]
+    }
+
 
 
 def does_path_exist(db_path):
