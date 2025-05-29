08c09eb7d0f9b216095ba26245b86adfd2360a8f
danielsf <scott.daniel@alleninstitute.org>
2025-05-29 08:33:31 -0700

    update prototype ensembl scraper

--- garage/ensembl_download/create_jsonld.py
+++ garage/ensembl_download/create_jsonld.py
@@ -1,218 +1,30 @@
-import ftplib
 import json
-import pandas as pd
-import pathlib
-import time
-import traceback
-from urllib.parse import urlparse
-import warnings
-
-from bkbit.models import genome_annotation as ga
-import bkbit.data_translators.genome_annotation_translator as gene_translator
-
-import mmc_gene_mapper.utils.file_utils as file_utils
-
-
-class Gff3Patch(gene_translator.Gff3):
-    """
-    Subclass of Gff3 that can write jsonld to a dict, rather
-    than print it to stdout
-    """
-
-    def serialize_to_jsonld(
-        self, exclude_none: bool = True, exclude_unset: bool = False
-    ):
-        """
-        Serialize the object and either write it to the specified output file or print it to the CLI.
-
-        Parameters:
-            exclude_none (bool): Whether to exclude None values in the output.
-            exclude_unset (bool): Whether to exclude unset values in the output.
-
-        Returns:
-            Dict
-
-        Notes
-        -----
-        Overrode default implementation so that we can return the dict
-        containing the jsonld graph
-        """
-
-        data = [
-            self.organism_taxon.dict(
-                exclude_none=exclude_none, exclude_unset=exclude_unset
-            ),
-            self.genome_assembly.dict(
-                exclude_none=exclude_none, exclude_unset=exclude_unset
-            ),
-            self.genome_annotation.dict(
-                exclude_none=exclude_none, exclude_unset=exclude_unset
-            ),
-        ]
-        for ck in self.checksums:
-            data.append(ck.dict(exclude_none=exclude_none, exclude_unset=exclude_unset))
-        for ga in self.gene_annotations.values():
-            data.append(ga.dict(exclude_none=exclude_none, exclude_unset=exclude_unset))
-
-        output_data = {
-            "@context": "https://raw.githubusercontent.com/brain-bican/models/main/jsonld-context-autogen/genome_annotation.context.jsonld",
-            "@graph": data,
-        }
-        return output_data
-
-
-    def parse_url(self):
-        """
-        Parses the content URL and extracts information about the genome annotation.
-
-        Returns:
-            A dictionary containing the following information:
-            - 'authority': The authority type (NCBI or ENSEMBL).
-            - 'taxonid': The taxon ID of the genome.
-            - 'release_version': The release version of the genome annotation.
-            - 'assembly_accession': The assembly accession of the genome.
-            - 'assembly_name': The name of the assembly.
-            - 'species': The species name (only for ENSEMBL URLs).
-
-        Notes
-        -----
-        Overrode default implementation so that we can parse URLs with
-        non-default file name schemas
-        """
-        # Define regex patterns for NCBI and Ensembl URLs
-        # NCBI : [assembly accession.version]_[assembly name]_[content type].[optional format]
-        # ENSEMBL :  <species>.<assembly>.<_version>.gff3.gz -> organism full name, assembly name, genome version
-        ncbi_pattern = r"/genomes/all/annotation_releases/(\d+)(?:/(\d+))?/(GCF_\d+\.\d+)[_-]([^/]+)/(GCF_\d+\.\d+)[_-]([^/]+)_genomic\.gff\.gz"
-        ensembl_pattern = (
-            r"/pub/release-(\d+)/gff3/([^/]+)/([^/.]+)\.([^/.]+)\.([^/.]+)\.gff3\.gz"
-        )
-
-        # Parse the URL to get the path
-        parsed_url = urlparse(self.content_url)
-        path = parsed_url.path
-
-        # Determine if the URL is from NCBI or Ensembl and extract information
-        if "ncbi" in parsed_url.netloc:
-            ncbi_match = re.search(ncbi_pattern, path)
-            if ncbi_match:
-                return {
-                    "authority": ga.AuthorityType.NCBI,
-                    "taxonid": ncbi_match.group(1),
-                    "release_version": (
-                        ncbi_match.group(2)
-                        if ncbi_match.group(2)
-                        else ncbi_match.group(4)
-                    ),
-                    "assembly_accession": ncbi_match.group(3),
-                    "assembly_name": ncbi_match.group(6),
-                }
-
-        elif "ensembl" in parsed_url.netloc:
-            file_path = pathlib.Path(parsed_url.path).name
-            file_path = file_path.replace('.gff3.gz', '')
-
-            scientific_name = file_path.split('.')[0]
-            file_path = file_path.replace(scientific_name+'.', '')
-            release_version = file_path.split('.')[-1]
-            file_path = file_path.replace('.' + release_version, '')
-            assembly_name = file_path
-
-            return {
-                "authority": ga.AuthorityType.ENSEMBL,
-                "release_version": release_version,
-                "scientific_name": scientific_name,
-                "assembly_name":assembly_name,
-            }
-
-        # If no match is found, return None
-        return None
-
+import mmc_gene_mapper.ensembl_download.scraper as scraper
 
 
 def main():
-    #df = pd.read_csv(
-    #    'data/20240125_gars_genome_annotation_tracking.csv'
-    #)
 
-    dst_dir = pathlib.Path('output')
+    dst_dir = 'output/'
 
-    t0 = time.time()
-    host = ftplib.FTP('ftp.ensembl.org')
-    host.login()
-    release_dir = 'pub/release-114/gff3'
-    directory_list = sorted(list(host.nlst(release_dir)))
-    failed_file_list = dict()
-    serialized_species = set()
-    for entry in directory_list:
-        print(entry,time.time()-t0)
-        file_path_list = list(host.nlst(entry))
-        chosen = None
-        for file_path in file_path_list:
-            if file_path.endswith('114.gff3.gz'):
-                assert chosen is None
-                chosen = file_path
-        assert chosen is not None
-        try:
-            serialize_bkbit_gff3(
-                content_url=f'https://ftp.ensembl.org/{chosen}',
-                assembly_id='placeholder.000',
-                dst_dir=dst_dir,
-                serialized_species=serialized_species
-            )
-        except Exception:
-            msg = traceback.format_exc()
-            print(f'    {chosen} failed -- {msg}')
-            failed_file_list[chosen] = msg
-    print(f'failed files\n{json.dumps(failed_file_list, indent=2)}')
-    dur = (time.time()-t0)/60.0
-    print(f'SUCCESS\nthat took {dur:.2e} minutes')
-    with open("jsonld_failures.json", "w") as dst:
-        dst.write(json.dumps(failed_file_list))
+    human = {
+        "url": "https://ftp.ensembl.org/pub/release-101/gff3/homo_sapiens/Homo_sapiens.GRCh38.101.gff3.gz",
+        "assembly_id": "NCBIAssembly:GCF_000001405.39"
+    }
 
-def serialize_bkbit_gff3(
-        content_url,
-        assembly_id,
-        dst_dir,
-        serialized_species):
+    mouse = {
+        "url": "https://ftp.ensembl.org/pub/release-98/gff3/mus_musculus/Mus_musculus.GRCm38.98.gff3.gz",
+        "assembly_id": "NCBIAssembly:GCF_000001635.26"
+    }
 
-    gff3 = Gff3Patch(
-        content_url=content_url,
-        assembly_accession=assembly_id,
-        assembly_strain=None,
-        log_level="WARNING",
-        log_to_file=False
+    valid_files = scraper.scrape_ensembl(
+        default_ensembl_version=114,
+        dst_dir=dst_dir,
+        failure_log_path="failures.20250528.json",
+        specific_files=[mouse, human]
     )
-    gff3.parse()
 
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore')
-        result = gff3.serialize_to_jsonld()
-
-    taxon = None
-    for element in result['@graph']:
-        if element['category'][0] == 'biolink:OrganismTaxon':
-            taxon = element
-    if taxon is None:
-        raise RuntimeError(
-            f"Could not find taxon for {content_url}"
-        )
-
-    taxon_id = taxon['id']
-    if taxon_id in serialized_species:
-        print(
-            f"    == skipping {content_url.split('/')[-1]}; "
-            "already serialized"
-        )
-        raise RuntimeError(f"{taxon_id} already serialized")
-
-    species_name = taxon['full_name'].lower().replace(' ', '_')
-    dst_path = dst_dir / f'{species_name}.jsonld'
-    if dst_path.exists():
-        print(f"duplicate {dst_path}")
-
-    with open(dst_path, 'w') as dst:
-        dst.write(json.dumps(result, indent=2))
-    serialized_species.add(taxon_id)
+    with open('valid_files.json', 'w') as dst:
+        dst.write(json.dumps(valid_files, indent=2))
 
 
 if __name__ == "__main__":
