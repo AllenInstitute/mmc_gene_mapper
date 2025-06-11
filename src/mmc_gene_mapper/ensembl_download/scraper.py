f36955eb68034babd017e08e8e2727ff8fb07466
danielsf <scott.daniel@alleninstitute.org>
2025-06-11 13:43:42 -0700

    add scraper

---
+++ src/mmc_gene_mapper/ensembl_download/scraper.py
@@ -0,0 +1,149 @@
+"""
+Define functions to scrape ENSEMBL ftp server for data
+"""
+import copy
+import ftplib
+import json
+import pathlib
+import time
+import traceback
+import warnings
+
+import mmc_gene_mapper.ensembl_download.gff3_class as gff3_class
+
+
+def scrape_ensembl(
+        default_ensembl_version,
+        dst_dir,
+        failure_log_path,
+        specific_files=None):
+    """
+    Parameters
+    ----------
+    default_ensembl_version:
+        an int. The default release version to download from ENSEMBL
+    dst_dir:
+        directory where jsonld files can be written
+    failure_log_path:
+        path to file where files that failed to process can be downloaded
+    specific_files:
+        optional list of specifications of specific versions of species
+        genome annotations to download. Each entry is a dict like
+        {'url': http://url/to/file.gff3.gz,
+         'assembly_id': 'id fo genome assembly'}
+
+    Returns
+    -------
+    A list of dicts. The data_file_spec to be passed to MMCGeneMapper for the
+    jsonld files produced
+    """
+    t0 = time.time()
+    dst_dir = pathlib.Path(dst_dir)
+    if not dst_dir.is_dir():
+        raise ValueError(
+            f"{dst_dir} is not a dir"
+        )
+
+    host = ftplib.FTP('ftp.ensembl.org')
+    host.login()
+    release_dir = f'pub/release-{default_ensembl_version}/gff3'
+    directory_list = sorted(list(host.nlst(release_dir)))
+
+    if specific_files is not None:
+        entry_list = copy.deepcopy(specific_files)
+    else:
+        entry_list = []
+
+    for ii, ftp_dir in enumerate(directory_list):
+        if ii > 0 and ii % 25 == 0:
+            print(f"considered {ii} dir; {len(entry_list)} entries")
+        file_path_list = list(host.nlst(ftp_dir))
+        chosen = None
+        for file_path in file_path_list:
+            if file_path.endswith(f'{default_ensembl_version}.gff3.gz'):
+                assert chosen is None
+                chosen = file_path
+        if chosen is not None:
+            this = {
+                'url': f'https://ftp.ensembl.org/{chosen}',
+                'assembly_id': 'placeholder.000'
+            }
+            entry_list.append(this)
+
+    print(f'processing {len(entry_list)} entries')
+
+    failed_file_lookup = dict()
+    serialized_species = set()
+    valid_files = []
+    for entry in entry_list:
+        print(entry, time.time()-t0)
+        try:
+            result_path = serialize_bkbit_gff3(
+                content_url=entry['url'],
+                assembly_id=entry['assembly_id'],
+                dst_dir=dst_dir,
+                serialized_species=serialized_species
+            )
+            valid_files.append(
+                str(result_path.resolve().absolute())
+            )
+        except Exception:
+            msg = traceback.format_exc()
+            print(f'    {chosen} failed -- {msg}')
+            failed_file_lookup[entry['url']] = msg
+
+    print(f'failed files\n{json.dumps(failed_file_lookup, indent=2)}')
+    dur = (time.time()-t0)/60.0
+    print(f'SUCCESS\nthat took {dur:.2e} minutes')
+    with open(failure_log_path, "w") as dst:
+        dst.write(json.dumps(failed_file_lookup))
+
+    return valid_files
+
+
+def serialize_bkbit_gff3(
+        content_url,
+        assembly_id,
+        dst_dir,
+        serialized_species):
+
+    gff3 = gff3_class.Gff3Patch(
+        content_url=content_url,
+        assembly_accession=assembly_id,
+        assembly_strain=None,
+        log_level="WARNING",
+        log_to_file=False
+    )
+    gff3.parse()
+
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore')
+        result = gff3.serialize_to_jsonld()
+
+    taxon = None
+    for element in result['@graph']:
+        if element['category'][0] == 'biolink:OrganismTaxon':
+            taxon = element
+    if taxon is None:
+        raise RuntimeError(
+            f"Could not find taxon for {content_url}"
+        )
+
+    taxon_id = taxon['id']
+    if taxon_id in serialized_species:
+        print(
+            f"    == skipping {content_url.split('/')[-1]}; "
+            "already serialized"
+        )
+        raise RuntimeError(f"{taxon_id} already serialized")
+
+    species_name = taxon['full_name'].lower().replace(' ', '_')
+    dst_path = dst_dir / f'{species_name}.jsonld'
+    if dst_path.exists():
+        raise RuntimeError(f"duplicate {dst_path}")
+
+    with open(dst_path, 'w') as dst:
+        dst.write(json.dumps(result, indent=2))
+    serialized_species.add(taxon_id)
+    dst_path = pathlib.Path(dst_path)
+    return dst_path
